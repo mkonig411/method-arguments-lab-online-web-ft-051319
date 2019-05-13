ca77d55ee8bfda3848953ecc3be76b96cee7fc4c
Margot Konig <mkonig411@gmail.com>
2019-05-13 18:15:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction_with_language(name, language)
-  put "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Margot","Ruby")
